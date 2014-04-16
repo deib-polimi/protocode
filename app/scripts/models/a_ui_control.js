2361ea7071a69a0f4963acc5a03f0570db98827b
Mattia Natali <matitalatina@gmail.com>
2014-04-16 14:00:50 +0200

    Added align parent properties, also added btn-group

--- app/scripts/models/a_ui_control.js
+++ app/scripts/models/a_ui_control.js
@@ -3,6 +3,10 @@ App.UiControl = DS.Model.extend({
   title: DS.attr('string'),
   posX: DS.attr('number'),
   posY: DS.attr('number'),
+  alignParentTop: DS.attr('boolean'),
+  alignParentBottom: DS.attr('boolean'),
+  alignParentStart: DS.attr('boolean'),
+  alignParentEnd: DS.attr('boolean'),
 
   viewController: DS.belongsTo('viewController'),
 
--- app/scripts/models/button.js
+++ app/scripts/models/button.js
@@ -4,6 +4,28 @@ App.Button = App.UiControl.extend({
 });
 
 App.Button.FIXTURES = [
-  {id: 1, name: 'Button1', title: 'Button 1', posX: 0, posY: 0, viewController: 1},
-  {id: 2, name: 'Button2', title: 'Button 2', posX: 80, posY: 60, viewController: 1}
+  {
+    id: 1,
+    name: 'Button1',
+    title: 'Button 1',
+    posX: 0,
+    posY: 0,
+    alignParentTop: true,
+    alignParentBottom: true,
+    alignParentStart: true,
+    alignParentEnd: true,
+    viewController: 1
+  },
+  {
+    id: 2,
+    name: 'Button2',
+    title: 'Button 2',
+    posX: 80,
+    posY: 60,
+    alignParentTop: false,
+    alignParentBottom: false,
+    alignParentStart: false,
+    alignParentEnd: false,
+    viewController: 1
+  }
 ];
