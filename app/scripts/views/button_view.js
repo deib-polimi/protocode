2f03c360417c61b193e76e78a329e8894af3d32e
Mattia Natali <matitalatina@gmail.com>
2014-04-18 14:10:13 +0200

    Fixed bug in control, they didn't expand themselves

--- app/scripts/views/button_view.js
+++ app/scripts/views/button_view.js
@@ -1,6 +1,6 @@
 App.ButtonView = Ember.View.extend({
   tagName: 'div',
-  classNames: ['control-button'],
+  classNames: ['control-button', 'expanded'],
   classNameBindings: ['controller.controllers.editor.platform'],
   templateName: 'views/button_view'
 
--- app/scripts/views/container_view.js
+++ app/scripts/views/container_view.js
@@ -1,6 +1,6 @@
 App.ContainerView = Ember.View.extend(App.UiDroppable, {
   tagName: 'div',
-  classNames: ['control-container'],
+  classNames: ['control-container', 'expanded'],
   classNameBindings: ['controller.controllers.editor.platform'],
   templateName: 'views/container_view'
 
