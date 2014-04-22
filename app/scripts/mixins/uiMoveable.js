062fadbae43139900f03e5be3695e9fbb4150820
Mattia Natali <matitalatina@gmail.com>
2014-04-22 12:17:56 +0200

    Improved movements with mouse

--- app/scripts/mixins/uiMoveable.js
+++ app/scripts/mixins/uiMoveable.js
@@ -1,19 +1,26 @@
 App.UiMoveable = Ember.Mixin.create({
   isMoving: false,
   classNameBindings: ['isMoving'],
+
+  offsetMouseX: 0,
+  offsetMouseY: 0,
   
   mouseMove: function(event) {
     if (this.get('isMoving')) {
       var element = this.get('element');
       var parentOffset = $(this.get('parentView.element')).offset();
-      this.set('context.posX', (event.pageX - parentOffset.left - element.offsetWidth / 2) / this.get('controller.zoomLevel'));
-      this.set('context.posY', (event.pageY - parentOffset.top - element.offsetHeight / 2) / this.get('controller.zoomLevel'));
+      this.set('context.posX', (event.pageX - parentOffset.left - this.get('offsetMouseX')) / this.get('controller.zoomLevel'));
+      this.set('context.posY', (event.pageY - parentOffset.top - this.get('offsetMouseY')) / this.get('controller.zoomLevel'));
     }
   },
 
   mouseDown: function(event) {
     event.preventDefault();
     this.set('isMoving', true);
+
+    var elementOffset = $(this.get('element')).offset();
+    this.set('offsetMouseX', event.pageX - elementOffset.left);
+    this.set('offsetMouseY', event.pageY - elementOffset.top);
     return false;
   },
 
