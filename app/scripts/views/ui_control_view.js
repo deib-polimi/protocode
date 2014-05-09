7d78375e44ab238aa26df80d86196f4d9f425168
Mattia Natali <matitalatina@gmail.com>
2014-05-09 17:33:05 +0200

    Linked CSS to properties

--- app/scripts/views/ui_control_view.js
+++ app/scripts/views/ui_control_view.js
@@ -28,27 +28,61 @@ App.UiControlView = Ember.View.extend(App.UiMoveable, {
   }.property('context.alignParentEnd'), 
 
   top: function() {
-    return this.get('context.top') / this.get('device.screenHeight') * this.get('device.cssHeight');
+    return this.computeVerticalAxis(this.get('context.top'));
   }.property('context.top', 'device'),
 
   bottom: function() {
-    return this.get('context.bottom') / this.get('device.screenHeight') * this.get('device.cssHeight');
+    return this.computeVerticalAxis(this.get('context.bottom'));
   }.property('context.bottom', 'device'),
 
   start: function() {
-    return this.get('context.start') / this.get('device.screenWidth') * this.get('device.cssWidth');
+    return this.computeHorizontalAxis(this.get('context.start'));
   }.property('context.start', 'device'),
 
   end: function() {
-    return this.get('context.end') / this.get('device.screenWidth') * this.get('device.cssWidth');
+    return this.computeHorizontalAxis(this.get('context.end'));
   }.property('context.end', 'device'),
 
+  /**** Margin ****/
+  marginTop: function() {
+    return this.computeVerticalAxis(this.get('context.marginTop'));
+  }.property('context.marginTop', 'device'),
+
+  marginBottom: function() {
+    return this.computeVerticalAxis(this.get('context.marginBottom'));
+  }.property('context.marginBottom', 'device'),
+
+  marginStart: function() {
+    return this.computeHorizontalAxis(this.get('context.marginStart'));
+  }.property('context.marginStart', 'device'),
+
+  marginEnd: function() {
+    return this.computeHorizontalAxis(this.get('context.marginEnd'));
+  }.property('context.marginEnd', 'device'),
+
+  /**** Padding ****/
+  paddingTop: function() {
+    return this.computeVerticalAxis(this.get('context.paddingTop'));
+  }.property('context.paddingTop', 'device'),
+
+  paddingBottom: function() {
+    return this.computeVerticalAxis(this.get('context.paddingBottom'));
+  }.property('context.paddingBottom', 'device'),
+
+  paddingStart: function() {
+    return this.computeHorizontalAxis(this.get('context.paddingStart'));
+  }.property('context.paddingStart', 'device'),
+
+  paddingEnd: function() {
+    return this.computeHorizontalAxis(this.get('context.paddingEnd'));
+  }.property('context.paddingEnd', 'device'),
+
   computedWidth: function() {
-    return this.get('context.computedWidth') / this.get('device.screenWidth') * this.get('device.cssWidth');
+    return this.computeHorizontalAxis(this.get('context.computedWidth'));
   }.property('context.computedWidth', 'device'),
 
   computedHeight: function() {
-    return this.get('context.computedHeight') / this.get('device.screenHeight') * this.get('device.cssHeight');
+    return this.computeVerticalAxis(this.get('context.computedHeight'));
   }.property('context.computedHeight', 'device'),
 
   uiControlType: function () {
@@ -68,40 +102,46 @@ App.UiControlView = Ember.View.extend(App.UiMoveable, {
 
   style: function() {
     var result = '';
-    /*
-    if (!this.get('alignParentTop') && !this.get('alignParentBottom')) {
-      result += this.get('posYStyle');
-    }
-    if (!this.get('alignParentStart') && !this.get('alignParentEnd')) {
-      result += this.get('posXStyle');
-    }
-    */
 
     result += 'top: ' + this.get('top') + 'px; ';    
     result += 'left: ' + this.get('start') + 'px; ';
     result += 'height: ' + this.get('computedHeight') + 'px;';
     result += 'width: ' + this.get('computedWidth') + 'px;';
-    
-    /*
-    if (!this.get('alignParentTop') || !this.get('alignParentBottom')) {
-      
-      result += 'height: ' + this.get('context.height') + 'px;';
-    }
 
-    if (!this.get('alignParentStart') || !this.get('alignParentEnd')) {
-      result += 'width: ' + this.get('context.width') + 'px;';
-    } */
+    result += 'margin-top: ' + this.get('marginTop') + 'px;';
+    result += 'margin-bottom: ' + this.get('marginBottom') + 'px;';
+    result += 'margin-left: ' + this.get('marginStart') + 'px;';
+    result += 'margin-right: ' + this.get('marginEnd') + 'px;';
+
+    result += 'padding-top: ' + this.get('paddingTop') + 'px;';
+    result += 'padding-bottom: ' + this.get('paddingBottom') + 'px;';
+    result += 'padding-left: ' + this.get('paddingStart') + 'px;';
+    result += 'padding-right: ' + this.get('paddingEnd') + 'px;';
 
     return result;
   }.property(
-    'context.start',
-    'context.top',
-    'context.alignParentTop',
-    'context.alignParentBottom',
-    'context.alignParentStart',
-    'context.alignParentEnd',
-    'context.width',
-    'context.height')
+    'start',
+    'top',
+    'computedWidth',
+    'computedHeight',
+    'marginTop',
+    'marginBottom',
+    'marginStart',
+    'marginEnd',
+    'paddingTop',
+    'paddingBottom',
+    'paddingStart',
+    'paddingEnd'
+    ),
+
+  computeVerticalAxis: function(value) {
+    return value / this.get('device.screenHeight') * this.get('device.cssHeight');
+  },
+
+  computeHorizontalAxis: function(value) {
+    return value / this.get('device.screenWidth') * this.get('device.cssWidth');
+
+  }
 
 
 
