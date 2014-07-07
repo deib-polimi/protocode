4a6bd90d7fd45dc1a936b8e3027a3518c8b9612a
Mattia Natali <matitalatina@gmail.com>
2014-07-07 11:45:55 +0200

    Added blob.js, starting implementing xml model generation

--- app/scripts/controllers/editor_controller.js
+++ app/scripts/controllers/editor_controller.js
@@ -1,2 +1,12 @@
 App.EditorController = Ember.ObjectController.extend({
+
+    actions: {
+      generateAppModel: function() {
+        var model = this.get('model').toXml();
+        var blob = new Blob(
+          ['<?xml version="1.0" encoding="ASCII"?>\n' + new XMLSerializer().serializeToString(model.documentElement)],
+          {type: "application/xml;charset=ASCII"});
+        saveAs(blob, "metamodel.xml");
+      }
+    }
 });
--- app/scripts/models/application.js
+++ app/scripts/models/application.js
@@ -2,7 +2,15 @@ App.Application = DS.Model.extend({
   name: DS.attr('string', {defaultValue: 'newApp'}),
   device: DS.belongsTo('device'),
   menu: DS.belongsTo('menu'),
-  viewControllers: DS.hasMany('viewController', {inverse: 'application'})
+  viewControllers: DS.hasMany('viewController', {inverse: 'application'}),
+
+  toXml: function() {
+    var xmlDocType = document.implementation.createDocumentType('appModel', 'MODEL', '<?xml version="1.0" encoding="ASCII"?>');
+    var xmlDoc = document.implementation.createDocument('appModelXml', null, xmlDocType);
+    var elem = xmlDoc.createElement("metamodel:Application");
+    xmlDoc.appendChild(elem);
+    return xmlDoc;
+  }
 });
 
 /*App.Application.FIXTURES = [
