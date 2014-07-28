815c26e10bb810ed135e20e46ca45ab8deb8b99f
Mattia Natali <matitalatina@gmail.com>
2014-07-28 15:43:39 +0200

    Update audioPlayer with new SourceType model

--- app/scripts/models/audio_player.js
+++ app/scripts/models/audio_player.js
@@ -16,10 +16,29 @@ App.AudioPlayer = App.UiControl.extend({
     var sourceType = this.get('sourceType');
 
     if (sourceType != null) {
-      elem.appendChild(sourceType.toXml(xmlDoc));
+      var sourceTypeAttrs = sourceType.toXml(xmlDoc).attributes;
+
+      for (var i = 0; i < sourceTypeAttrs.length; i++) {
+        var attr = sourceTypeAttrs[i];
+        elem.setAttribute(attr.name, attr.value);
+      };
     }
     
     return elem;
+  },
+
+  // Override because there's only one videoView
+  getRefPath: function(path) {
+    var updatedPath = '/@' + this.get('xmlName');
+
+    if (this.get('parentContainer') != null) {
+      updatedPath = this.get('parentContainer').getRefPath(updatedPath);
+    }
+    else {
+      updatedPath = this.get('viewController').getRefPath(updatedPath);
+    }
+
+    return updatedPath;
   }
 });
 /*
