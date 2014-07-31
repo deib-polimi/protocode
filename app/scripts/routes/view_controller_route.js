15676259ec1611f319c8fee6717bfa46e2f463f7
Mattia Natali <matitalatina@gmail.com>
2014-07-31 10:55:13 +0200

    Clean coding

--- app/scripts/routes/view_controller_route.js
+++ app/scripts/routes/view_controller_route.js
@@ -12,49 +12,11 @@ App.ViewControllerRoute = Ember.Route.extend({
       console.log('Receiver of drop event: ' + receiver.get('context.name'));
       console.log('Type of receiver: ' + receiver.get('context').constructor.toString());
 
-      var uiControl = {};
-
       var uiControl = this.store.createRecord(controlType, {
         viewController: this.get('controller.model')
       });
 
       this.get('controller.model').save();
-
-      /*
-      switch (controlType) {
-        case 'button':
-          var uiControl = this.store.createRecord('button', {
-            name: 'DummyButtonName',
-            title: 'Dummy Button',
-            viewController: this.get('controller.model')
-          });
-          break;
-
-        case 'container':
-          var uiControl = this.store.createRecord('container', {
-            name: 'DummyContainer',
-            title: 'Dummy Container',
-            viewController: this.get('controller.model')
-          });
-          break;
-
-        case 'editText':
-          var uiControl = this.store.createRecord('editText', {
-            name: 'DummyTextEdit',
-            title: 'Dummy Text Edit',
-            viewController: this.get('controller.model')
-          });
-          break;
-
-        case 'label':
-          var uiControl = this.store.createRecord('label', {
-            name: 'DummyLabel',
-            title: 'Dummy Label',
-            viewController: this.get('controller.model')
-          });
-          break;
-      }
-      */
 
       if (receiver.get('context').constructor.toString() == 'App.Container') {
         uiControl.set('parentContainer', receiver.get('context'));
