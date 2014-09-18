1e3e51170ab92c938efecca24f1ad65859cc0fce
Mattia Natali <matitalatina@gmail.com>
2014-09-18 10:46:38 +0200

    Changed encoding of protocode model

--- app/scripts/controllers/editor_controller.js
+++ app/scripts/controllers/editor_controller.js
@@ -3,7 +3,7 @@ App.EditorController = Ember.ObjectController.extend(App.Saveable, {
     actions: {
       generateAppModel: function() {
         this.get('model').toXml().then(function (model) {
-          var xmlString = '<?xml version="1.0" encoding="ASCII"?>\n' + new XMLSerializer().serializeToString(model.documentElement);
+          var xmlString = '<?xml version="1.0" encoding="UTF-8"?>\n' + new XMLSerializer().serializeToString(model.documentElement);
           var blob = new Blob(
             [vkbeautify.xml(xmlString)],
             {type: "application/xml;charset=ASCII"});
